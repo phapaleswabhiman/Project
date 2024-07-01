3c6b194960182dcbd86974f0ddaceaa7ccf71e26
phapaleswabhiman <swabhimanphapale24@gmail.com>
2024-07-01 15:58:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const swabhi = document.querySelector(".wrapper h1");
+console.log(swabhi);
+
+const image = document.querySelector(".wrapper img");
+console.log(image);
+
+heading.removeAttribute("id", "design");
